e885296b175346aa8318f2658e3002c66241f9f6
Katharine Doyle <katharinecdoyle@gmail.com>
2019-02-24 13:44:49 +0000

    Added about page

--- js/home.js
+++ js/home.js
@@ -14,7 +14,7 @@
 // Way to maximise performance
 
 var activeDropdown = {};
-document.getElementById('icecream-dropdown').addEventListener('mouseover',function(){
+document.getElementById('icecream-dropdown').addEventListener('click',function(){
   for (var i = 0;i<this.children.length;i++){
     if (this.children[i].classList.contains('dropdown-selection')){
         // saving data to object, can recall easily
@@ -26,7 +26,7 @@ document.getElementById('icecream-dropdown').addEventListener('mouseover',functi
 });
 
 
-window.onmouseover = function(event){
+window.onclick = function(event){
   if (!event.target.classList.contains('dropdown-button')){
     activeDropdown.element.style.visibility = 'hidden';
   }
