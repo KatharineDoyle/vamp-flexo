587184c619a6f94fdeb7b42bfd4f5b209cd05a15
Katharine Doyle <katharinecdoyle@gmail.com>
2019-03-04 17:14:28 +0000

    Navbar fade on scroll

--- js/home.js
+++ js/home.js
@@ -1,23 +1,8 @@
-// First attempt creating dropdown
-
-// document.getElementById('icecream-dropdown').addEventListener('mouseover',function(){
-// //looping over all the children of dropdown
-//   for (var i = 0; i < this.children.length ; i++){
-//     //checking if one of children is dropdown-selection
-//     if (this.children[i].classList.contains('dropdown-selection')){
-//         //only then add the inline style
-//         this.children[i].style.visibility = 'visible';
-//         }
-//   }
-// });
-
-// Way to maximise performance
-
 var activeDropdown = {};
 document.getElementById('icecream-dropdown').addEventListener('click',function(){
   for (var i = 0;i<this.children.length;i++){
     if (this.children[i].classList.contains('dropdown-selection')){
-        // saving data to object, can recall easily
+        //saving the data into our object, so we can recall it easily
         activeDropdown.id = this.id;
         activeDropdown.element = this.children[i];
         this.children[i].style.visibility = 'visible';
@@ -25,43 +10,42 @@ document.getElementById('icecream-dropdown').addEventListener('click',function()
   }
 });
 
-
 window.onclick = function(event){
   if (!event.target.classList.contains('dropdown-button')){
     activeDropdown.element.style.visibility = 'hidden';
   }
 }
 
-////// Image carousel
+//// Image carousel
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 var slideIndex = 0;
 showSlides();
@@ -75,5 +59,6 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+// $(function () {
+  $(document).scroll(function () {
+    var $nav = $("#navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+// });
