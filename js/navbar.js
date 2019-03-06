821a47302864e47cdc1ed6f648b12a266790dd88
Katharine Doyle <katharinecdoyle@gmail.com>
2019-03-06 13:53:47 +0000

    Added rotating fangs logo

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-
-// $(function () {
   $(document).scroll(function () {
     var $nav = $("#navbar");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
+
+$(window).scroll(function() {
+var theta = $(window).scrollTop() / 10 % Math.PI;
+$('#yellow-vamp-logo').css({ transform: 'rotate(' + theta + 'rad)' });
 });
-// });
+});
